7077c88e1e1ed469ba4a24d2004211ce9c17854e
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-10-26 15:44:09 -0300

    Atividade concluída

---
+++ java.js
@@ -0,0 +1,5 @@
+let nome = prompt("digite o seu nome:");
+let sobrenome = prompt("digite o seu spbrenome:");
+
+let nomeCompleto = alert(`Nome completo: ${nome} ${sobrenome}`);
+let nomeCatalogo = alert(`Nome em catálogo: ${sobrenome.toUpperCase()}, ${nome}`)
